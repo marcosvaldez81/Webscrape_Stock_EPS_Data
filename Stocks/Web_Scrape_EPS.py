295cf3e19e8459b94b44041da74a47d84cf14646
Marcos Valdez <marcos.valdez619@gmail.com>
2022-04-01 23:23:54 -0700

    Working code, prints nicely

--- Stocks/Web_Scrape_EPS.py
+++ Stocks/Web_Scrape_EPS.py
@@ -9,74 +9,62 @@
 
 from bs4 import BeautifulSoup
 import requests 
-import json
 
-
-ticker = "PSTG"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 Safari/537.36'}   
-#url = "https://finance.yahoo.com/quote/PSTG?p=PSTG&.tsrc=fin-srch"
 
-url = "https://finance.yahoo.com/quote/PSTG"
-url2 = "https://finance.yahoo.com/quote/PSTG/analysis?p=PSTG"
+def get_eps(ticker):
+    """ This function will take passed in ticker and  """
+    
+    url2 = "https://finance.yahoo.com/quote/" + ticker + "/analysis?p=" + ticker
 
-response = requests.get(url2, headers = headers)
-soup = BeautifulSoup(response.content, 'lxml') #lxml is the best parser for web scraping
+    response = requests.get(url2, headers = headers)
 
-header = soup.find_all("div", id= "Main")[0]
-#print(header)
-
-
-
-test = header.find("div", id="mrt-node-Col1-0-AnalystLeafPage")
-
-#print(test)
-
-table = test.find("div", id= "Col1-0-AnalystLeafPage-Proxy")
-
-earnings_history = table.find_all("table",class_="W(100%) M(0) BdB Bdc($seperatorColor) Mb(25px)")[2]
-earnings_history_title = table.find_all("table",class_="W(100%) M(0) BdB Bdc($seperatorColor) Mb(25px)")[2].find("span").get_text()
-
-dates = table.find_all("table",class_="W(100%) M(0) BdB Bdc($seperatorColor) Mb(25px)")[2].find_all("tr")
-
-dates_list = dates[0].find_all("span") # list of date values from table
-
-
-print(earnings_history_title, "for stock = ")
-
-
-# would need to do a double for loop maybe, could format the print statements.
-
-for i in range(1,len(dates_list)):
-    print(dates_list[i].get_text())
-
-
-
-print(dates[1].find_all("span"))
-
-
-# def get_EPS(ticker):
-#     """ This function is to fetch recent EPS data from passed in ticker """
-#     ticker = str(ticker)
-#     
-#     url = "https://finance.yahoo.com/quote/" + ticker + "/analysis?p=" + "PSTG"
-#     request = requests.get(url)
-# #     if request:
-# #         print("works")
-# #     else:
-# #         print("err")
-
-
-
-
+    soup = BeautifulSoup(response.content, 'lxml') #lxml is the best parser for web scraping
+    
+    header = soup.find_all("div", id= "Main")[0]
+    
+    test = header.find("div", id="mrt-node-Col1-0-AnalystLeafPage")
+    table = test.find("div", id= "Col1-0-AnalystLeafPage-Proxy")
+    earnings_history_title = table.find_all("table",class_="W(100%) M(0) BdB Bdc($seperatorColor) Mb(25px)")[2].find("span").get_text()
+    earnings_history_table = table.find_all("table",class_="W(100%) M(0) BdB Bdc($seperatorColor) Mb(25px)")[2].find_all("tr")
+    dates_list = earnings_history_table[0].find_all("span") # list of date values from table
+    
+    # eps estimate row
+    estimate = earnings_history_table[1].find_all("span")
+    eps_estimate_title = estimate[0].get_text()
+    
+    print(earnings_history_title, "for ticker = $" + str(ticker))
+    print()
+    
+    print("\t\t",dates_list[1].get_text() ,"\t", dates_list[2].get_text(), "\t",
+          dates_list[3].get_text(),"\t", dates_list[4].get_text())
+    
+    print("-----" * 15)
+    print()
+    
+    # eps estimate row
+    print(eps_estimate_title, "\t ", estimate[1].get_text(), "  \t  ", estimate[2].get_text(),
+           "    \t  ", estimate[3].get_text(), "    \t  ", estimate[4].get_text())
+    
+    # eps actual row
+    eps_actual = earnings_history_table[2].find_all("span")
+    eps_actual_title = eps_actual[0].get_text()
+    print(eps_actual_title,"    \t ", eps_actual[1].get_text(),"     \t  ", eps_actual[2].get_text(),
+           "    \t  ", eps_actual[3].get_text(), "    \t  ", eps_actual[4].get_text())
+    
+    
+    #difference Row:
+    difference = earnings_history_table[3].find_all("span")
+    difference_title = difference[0].get_text()
+    print(difference_title,"    \t ", difference[1].get_text(), "     \t  ", difference[2].get_text(),
+           "    \t  ", difference[3].get_text(), "    \t  ", difference[4].get_text())
+    # surprise row
+    surprise = earnings_history_table[4].find_all("span")
+    surprise_title = surprise[0].get_text()
+    print(surprise_title,"    \t", surprise[1].get_text(), "    \t  ", surprise[2].get_text(),
+           "    \t  ", surprise[3].get_text(), "    \t  ", surprise[4].get_text())
 
 
-# 
-# response = requests.get(url, headers = headers)
-# if response:
-#     print("yes")
-#     
-#     soup = BeautifulSoup(response.content, 'lxml') #lxml is the best parser for web scraping
-#     print(soup.title)
-#     
-# else:
-#     print("err")
+
+get_eps("AAPL")
+
